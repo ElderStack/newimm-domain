338b1c222b0d21fcc1623f88e64372a3be8843bf
ElderStack <jse54664@gmail.com>
2025-03-04 14:32:44 -0500

    Immigrant Update
    Added toString and getter and setter methods

--- newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
@@ -3,8 +3,28 @@ package edu.gmu.cs321;
 public class Immigrant {
     private String name;
     private int ID;
-    private Immigrant(String name, int ID) {
+    public Immigrant(String name, int ID) {
         this.name = name;
+        this.ID = ID;
+    }
+
+    public String toString(){
+        return "Name: " + this.name + ", ID: " + this.ID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getID(){
+        return this.ID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setID(int ID){
         this.ID = ID;
     }
 }
