43ee338dc90a9a51414b5fccf604daba26aaffae
GramCake <oceangodgamer@gmail.com>
2025-03-04 14:28:38 -0500

    Immigrant Constructor
    I made an constructor for Immigrant

--- newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
@@ -1,5 +1,10 @@
 package edu.gmu.cs321;
 
 public class Immigrant {
-
+    private String name;
+    private int ID;
+    private Immigrant(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
 }
