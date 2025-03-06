d5b3aedaa15137bd318aac4b124a9d4756a28be1
GramCake <oceangodgamer@gmail.com>
2025-03-06 14:21:46 -0500

    Test Class
    created a Test class with improts

---
+++ newimm-domain/src/test/java/edu/gmu/cs321/Test.java
@@ -0,0 +1,9 @@
+package edu.gmu.cs321;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.TestClassOrder;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Test
+
+public class Test {
+
+}
