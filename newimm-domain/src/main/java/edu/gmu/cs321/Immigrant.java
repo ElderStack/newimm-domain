f2416e549df479cd7ed22881d088e0b129141ed9
ElderStack <jse54664@gmail.com>
2025-03-20 16:32:28 -0400

    Added DoB and address to Immigrant
    Adding in testers, not finished gonna do that later

--- newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/Immigrant.java
@@ -15,23 +15,43 @@ public class Immigrant {
     private int ID;
 
     /**
+     * Date of Birth of the Immigrant
+     */
+    private String DoB;
+
+    /**
+     * Address of the Immigrant
+     */
+    private String address;
+
+    /**
      * Default Constructor.
      * - name = null
      * - ID = -1
+     * - address = null
+     * - DoB = null
      */
     public Immigrant() {
         this.name = null;
         this.ID = -1;
+        this.address = null;
+        this.DoB = null;
     }
 
     /**
      * Parameterized Constructor
-     * @param name  Name of the Immigrant
-     * @param ID    Unique ID of the Immigrant
+     * @param name      Name of the Immigrant
+     * @param ID        Unique ID of the Immigrant
+     * @param address   Address of the Immigrant
+     * @param DoB       Date of Birth of the Immigrant
      */
-    public Immigrant(String name, int ID) {
+    public Immigrant(String name, int ID, String address, String DoB) {
         this.name = name;
         this.ID = ID;
+        this.address = address;
+        if (verifyDoB(DoB)){
+            this.DoB = DoB;
+        }
     }
 
     /**
@@ -72,5 +92,76 @@ public class Immigrant {
      */
     public void setID(int ID) {
         this.ID = ID;
+    }
+
+    /**
+     * Returns Immigrant address
+     * @return
+     */
+    public String getAddress() {
+        return this.address;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getDoB() {
+        return this.DoB;
+    }
+
+    /**
+     * Sets Immigrant Address
+     */
+    public void setAddress(String newAddress) {
+        this.address = newAddress;
+    }
+
+    /**
+     * Sets Immigrant Date of Birth
+     */
+    public void setDob(String newDoB){
+        if (verifyDoB(newDoB)){
+            this.DoB = newDoB;
+        }
+    }
+
+    /**
+     * Verifies that the String submitted as the date of birth is formatted correctly
+     * @return ture if date of birth is submitted correctly.
+     */
+    private boolean verifyDoB(String DoB) {
+        String[] splitDate = DoB.split("/");
+        //checks lengths of the strings first
+        if (splitDate.length == 3 && splitDate[0].length() == 4 && splitDate[1].length() == 2 && 
+        splitDate[2].length() == 2) {
+            //checks each string to see if its numeric
+            for (String item: splitDate){
+                if (!isNumeric(item)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Helper method that checks if a string is numeric
+     * @param str String to be checked
+     * @return True if number is numeric
+     */
+    private boolean isNumeric(String str) {
+        if (str == null){
+            return false;
+        }
+
+        try {
+            double num = Double.parseDouble(str);
+            return true;
+        } 
+        catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- newimm-domain/src/test/java/edu/gmu/cs321/ImmigrantTest.java
+++ newimm-domain/src/test/java/edu/gmu/cs321/ImmigrantTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.TestClassOrder;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Class to run tests on Immigrant.java
+ */
 public class ImmigrantTest {
-    private String name = "Karl";
-    private int id = 1;
-    private Immigrant testImm = new Immigrant(this.name, this.id);
+    private final String name = "Karl";
+    private final int id = 1;
+    private final String address = "1234 Lane";
+    private final String DoB = "2000/01/01";
+    private Immigrant testImm = new Immigrant(this.name, this.id, this.address, this.DoB);
 
-    /**
+    /** ADD ADDRESS AND DOB
      * Test Immigrant.java getter functions.
      * Test Values:
      * - name = "Karl"
@@ -25,7 +30,7 @@ public class ImmigrantTest {
         assertTrue(testGetId);
     }
 
-    /**
+    /** ADD ADDRESS AND DOB
      * Test Immigrant.java setter functions. 
      * Test Values:
      * - name = "Big Boss"
@@ -41,7 +46,7 @@ public class ImmigrantTest {
         testImm.setID(testID);
         assertTrue(testImm.getID() == testID);
 
-        testImm = new Immigrant(this.name, this.id);
+        testImm = new Immigrant(this.name, this.id, this.address, this.DoB);
     }
 
     /**
@@ -53,22 +58,30 @@ public class ImmigrantTest {
      * Test Values for parameterized constructor:
      * - name = "Ocelot"
      * - id = 7
+     * - address = 6758 Victory Road
+     * - DoB = 3000/01/01
      */
     @Test
     public void testConstructor() {
         String testName = "Ocelot";
         int testID = 7;
-        Immigrant newImm = new Immigrant(testName, testID);
+        String testAddress = "6758 Victory Road";
+        String testDoB = "3000/01/01";
+        Immigrant newImm = new Immigrant(testName, testID, testAddress, testDoB);
         
         assertTrue(newImm.getName().equals(testName));
         assertTrue(newImm.getID() == testID);
+        assertTrue(newImm.getAddress().equals(testAddress));
+        assertTrue(newImm.getDoB().equals(testDoB));
 
         newImm = new Immigrant();
         assertTrue(newImm.getName() == (null));
         assertTrue(newImm.getID() == -1);
+        assertTrue(newImm.getAddress() == (null));
+        assertTrue(newImm.getDoB() == (null));
     }
 
-    /**
+    /** ADD ADDRESS AND DoB
      * Tests Immigrant.java toString()
      * Test values:
      * - name = "Karl"
