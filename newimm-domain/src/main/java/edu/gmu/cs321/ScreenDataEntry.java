7d43eb058e0506a106144a12ec3f9710ef769e88
ElderStack <jse54664@gmail.com>
2025-04-03 15:59:03 -0400

    added javafx to pom

--- newimm-domain/src/main/java/edu/gmu/cs321/ScreenDataEntry.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/ScreenDataEntry.java
@@ -1,5 +1,28 @@
 package edu.gmu.cs321;
 
-public class ScreenDataEntry {
-    
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import static javafx.geometry.HPos.RIGHT;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ScreenDataEntry extends Form{
+    public ScreenDataEntry(){
+        super();
+    }
+
+
 }
