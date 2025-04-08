ac73ff728391f8124f419c05fa33fe2dd3bd5bf1
ElderStack <jse54664@gmail.com>
2025-04-08 13:34:17 -0400

    Update ScreenDataEntry.java

--- newimm-domain/src/main/java/edu/gmu/cs321/ScreenDataEntry.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/ScreenDataEntry.java
@@ -1,38 +1,3 @@
-/*
- * Copyright (c) 2012, 2014 Oracle and/or its affiliates.
- * All rights reserved. Use is subject to license terms.
- *
- * This file is available and licensed under the following license:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *  - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the distribution.
- *  - Neither the name of Oracle nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * Modified by Jakob Elmore
- */
 package edu.gmu.cs321;
 
 import javafx.application.Application;
@@ -54,94 +19,24 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-
-public class ScreenDataEntry extends Application {
-    private GridPane grid = new GridPane();
-    private int textBoxCounter = 1;
-    private ArrayList<TextField> textFields;
-
-    private void addTextBox(String text){
-        Label label = new Label(text);
-        grid.add(label, 0, textBoxCounter);
-
-        TextField textBox = new TextField();
-        grid.add(textBox, 1, textBoxCounter);
-        textFields.add(textBox);
-        textBoxCounter++;
-    }
-
-    private Immigrant createImmigrant(ArrayList<TextField> textFields){
-        Immigrant newImm = new Immigrant();
-        newImm.setName(textFields.get(0).getText() + " " + textFields.get(1).getText());
-        newImm.setDoB(textFields.get(2).getText());
-        newImm.setAddress(textFields.get(3).getText());
-        return newImm;
-    }
+public class ScreenDataEntry extends Application{
+    Form form = new Form();
 
     @Override
-    public void start(Stage primaryStage) {
-        textFields = new ArrayList<TextField>();
-
-        primaryStage.setTitle("JavaFX Welcome");
-        grid.setAlignment(Pos.TOP_LEFT);
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Data Entry Form");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
-
-        addTextBox("First Name: ");
-
-        addTextBox("Last Name: ");
-
-        addTextBox("Date of Birth: ");
-
-        addTextBox("Address: ");
-
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 0, 8);
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 0, 6);
-        grid.setColumnSpan(actiontarget, 2);
-        grid.setHalignment(actiontarget, RIGHT);
-        actiontarget.setId("actiontarget");
-
-        // Immigrant newImm;
-
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
-
-                for (TextField field: textFields){
-                    if (field.lengthProperty().intValue() == 0){
-                        actiontarget.setText("Missing a field");
-                        return;
-                    }
-                }
-
-                //newImm = createImmigrant(textFields);
-                actiontarget.setText("Successfully submitted the form.");
-
-
-            }
-        });
-
-        Scene scene = new Scene(grid, 600, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Text sceneTitle = new Text("Form Retrieval");
+        sceneTitle.setFont(Font.font("Monocraft", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
     }
-
 }
