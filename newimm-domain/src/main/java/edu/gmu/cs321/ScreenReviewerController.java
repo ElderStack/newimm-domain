1a09cb2ff301e41cc28a1f65a400189ed3ea5595
LWDuong <109881149+LWDuong@users.noreply.github.com>
2025-04-22 20:54:50 -0400

    Reviewer non-functional methods for future implementation

--- newimm-domain/src/main/java/edu/gmu/cs321/ScreenReviewerController.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/ScreenReviewerController.java
@@ -16,5 +16,19 @@ public class ScreenReviewerController {
     void initialize() {
 
     }
-
+    /**
+     * Receives information from form input and loads into reviewer screen to review.
+     * @return if the data was correctly loaded
+     */
+    public boolean loadData() {
+        return false;
+    }
+    /**
+     * Based on manual check, reviewer will send back judgement to the workflow. If information is acceptable,
+     * continues to next workflow step. If not, goes back into data entry.
+     * @return if the information was passable
+     */
+    public boolean sendReview() {
+        return false;
+    }
 }
